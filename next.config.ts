df440dca007928183c739b798b87ddf841e32617
konstantinos193 <96600944+konstantinos193@users.noreply.github.com>
2024-12-14 20:21:25 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,21 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: "/(.*)",
-        has: [
-          {
-            type: "host",
-            value: "deape.fi", // Only apply redirect for the non-HTTPS version
-          },
-        ],
-        destination: "https://deape.fi/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
